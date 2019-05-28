173cd61f91c5066d856c1c969ab411943617cc60
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-28 14:52:33 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(full_name)
+    @name = full_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job_name)
+    @job = job_name
+  end
+
+  def job
+    @job
+  end
+
+end
